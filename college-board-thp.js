7ccecfd676201ebc398053b1c256c9aa1b56779b
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-18 15:46:42 -0600

    toeplitz code challenge

---
+++ college-board-thp.js
@@ -0,0 +1,30 @@
+/*
+abubanggheed — Today at 3:17 PM
+Write a function that takes in at least 1 function as an input and returns a new function
+that is the composition of the inputs.
+e.g.
+compose(
+  x => x - 1,
+  x => x * 2,
+  x => x + ' s'
+)(10)
+'18 s'
+
+compose(
+  arg => arg.reduce((a, b) => a + b),
+  x => x * x
+)([-7, 1, 8])
+4
+*/
+
+function compose([fun]) {
+    // store input
+
+}
+
+// takes in functions, returns one callback function?
+
+
+
+
+
---
+++ toeplitz.js
@@ -0,0 +1,44 @@
+// Prompt
+// A Toeplitz Matrix is one where the values on every diagonal are the same: Given a 2d matrix (multidimensional array), return true if the input is a Toeplitz matrix and false otherwise.
+
+ 
+
+// Example of a valid Toeplitz matrix:
+
+//     1 2 3 4
+//     5 1 2 3
+//     6 5 1 2
+//     7 6 5 1
+
+function isToeplitz(m) {
+    /* your code here */
+    if (m.length == 0) {
+        return false;
+    } else {
+        for (let i = 0; i < m.length; i++) {
+            let compVal = m[0][i];
+            for (let j=0; j+i < m.length; j++) {
+                console.log('compval: ', compVal);
+                console.log('to : ', m[j][j+i], j);
+                if (m[j][j+i] !== compVal) {
+                    return false;
+                }
+            }
+        }
+        for (let i = 1; i < m.length; i++) {
+            let compVal = m[i][0];
+            for (let j=1; j+i < m.length; j++) {
+                console.log('compval: ', compVal);
+                console.log('to : ', m[j+i][j], j);
+                if (m[j+i][j] !== compVal) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+let m = [[1 ,2, 3, 4], [5, 1, 2, 3], [6, 5, 1, 2], [7, 6, 5, 1]];
+
+console.log(isToeplitz(m));

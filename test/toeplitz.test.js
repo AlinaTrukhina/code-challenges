f6126f48a660b34765e7e8925a825a5763413bbd
AlinaTrukhina <alina.trukhina@gmail.com>
2023-02-18 16:21:46 -0600

    add toeplitz tests

---
+++ test/toeplitz.test.js
@@ -0,0 +1,19 @@
+const isToeplitz = require('../toeplitz');
+
+describe('toeplitz', () => {
+    test('returns true', () => {
+        expect(isToeplitz([[1 ,2, 3, 4], [5, 1, 2, 3], [6, 5, 1, 2], [7, 6, 5, 1]])).toBeTruthy()
+    });
+
+    test('returns true', () => {
+        expect(isToeplitz([[2 ,2, 3, 4], [5, 1, 2, 3], [6, 5, 1, 2], [7, 6, 5, 1]])).toBeFalsy()
+    });
+
+    test('chars, returns false', () => {
+        expect(isToeplitz([['a', 'a'],['b', 'a']])).toBeTruthy();
+    });
+
+    test('chars, returns false', () => {
+        expect(isToeplitz([[]])).toBeFalsy();
+    });
+});
--- toeplitz.js
+++ toeplitz.js
@@ -1,7 +1,6 @@
 // Prompt
-// A Toeplitz Matrix is one where the values on every diagonal are the same: Given a 2d matrix (multidimensional array), return true if the input is a Toeplitz matrix and false otherwise.
-
- 
+// A Toeplitz Matrix is one where the values on every diagonal are the same: 
+// Given a 2d matrix (multidimensional array), return true if the input is a Toeplitz matrix and false otherwise.
 
 // Example of a valid Toeplitz matrix:
 
@@ -12,7 +11,7 @@
 
 function isToeplitz(m) {
     /* your code here */
-    if (m.length == 0) {
+    if (m.length == 0 || m[0].length == 0) {
         return false;
     } else {
         for (let i = 0; i < m.length; i++) {
@@ -28,8 +27,8 @@ function isToeplitz(m) {
         for (let i = 1; i < m.length; i++) {
             let compVal = m[i][0];
             for (let j=1; j+i < m.length; j++) {
-                console.log('compval: ', compVal);
-                console.log('to : ', m[j+i][j], j);
+                // console.log('compval: ', compVal);
+                // console.log('to : ', m[j+i][j], j);
                 if (m[j+i][j] !== compVal) {
                     return false;
                 }
@@ -39,6 +38,10 @@ function isToeplitz(m) {
     return true;
 }
 
-let m = [[1 ,2, 3, 4], [5, 1, 2, 3], [6, 5, 1, 2], [7, 6, 5, 1]];
+// let m = [[1 ,2, 3, 4], [5, 1, 2, 3], [6, 5, 1, 2], [7, 6, 5, 1]];
 
-console.log(isToeplitz(m));
+// console.log(isToeplitz(m));
+let m = [[]];
+console.log(m[0]);
+
+module.exports = isToeplitz;
